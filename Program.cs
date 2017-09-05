8119ee410ab2e773967cc529db5dd6c3e7a0ec6c
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-05 12:41:44 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
