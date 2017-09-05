fba08d6a1d44c06423d46b69ccb691debea85ab3
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-05 12:42:10 -0500

    completed project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
-namespace ForLoops
+namespace Loops
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var name = "John Smith";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach (var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //var numbers = new int[] { 1, 2, 3, 4 };
+
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 ==0) 
+            //    {
+            //        Console.WriteLine(i);
+            //        i++;
+            //    }
+            //}
+
+            //case where while loop would make sense
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@ECHO: " + input);
+                    continue; //returns to the beginning of the loop
+                }
+                break;
+
+            }
         }
     }
 }
